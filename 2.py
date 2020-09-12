45e1a745e56d14efbb67ab6921d10822ada07215
beiyue <1041184302@qq.com>
2020-09-12 13:19:14 +0800

    the five plus version at company development

--- 2.py
+++ 2.py
@@ -1,5 +1,5 @@
 print('the new functtion development for the two versionbase')
 print('use dev branch to development new fuction for the three version')
 print('at home在家写代码')
-
+print('at company development 在公司里面开发的代码')
 
